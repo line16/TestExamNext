bef0ae14f91e964147b17aed85a9b8a4e33db43e
line16 <linesorensen53@gmail.com>
2024-09-17 23:29:54 +0200

    frontpage design almost done

--- src/app/Contact/BookAtime/page.jsx
+++ src/app/Contact/BookAtime/page.jsx
@@ -1,9 +1,118 @@
-import React from 'react'
+"use client";
+import React, { useEffect } from "react";
+import useRequestData from "../../../../Hooks/useRequestData";
+import Image from "next/image";
 
 const BookAtime = () => {
-  return (
-    <div>BookAtime</div>
-  )
-}
+  const { data, isLoading, error, makeRequest } = useRequestData();
+  const { dataPic, isLoadingPic, errorPic, makeRequestPic } = useRequestData();
 
-export default BookAtime
+  useEffect(() => {
+    makeRequest("http://localhost:5029/appointment/");
+  }, []);
+
+  // useEffect(() => {
+  //   makeRequestPic("http://localhost:5029/treatment")
+  // }, [])
+
+  return (
+    <>
+      <div className="flex flex-row justify-center py-7">
+        <div className="lg:w-2/6 bg-rose-100 h-auto">
+        <Image 
+        src="/spa.png" width={450} height={250}></Image></div>
+
+        <div className="lg:w-4/6 bg-rose-100 py-2 pl-12 lg:pr-36">
+          <form className="py-14">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+              {/* Name */}
+              <div className="relative border-b-2 border-y-gray-300 w-full">
+                <label className="hidden">NAME</label>
+                <input
+                  type="text"
+                  className="px-4 py-4  bg-rose-100 mt-1 rounded-md focus:outline-none focus:ring-2 focus:ring-rose-300 focus:border-transparent"
+                  placeholder="NAME"
+                  required
+                />
+              </div>
+
+              {/* Email Address */}
+              <div className="relative border-b-2 border-y-gray-300 w-full">
+                <label className="hidden">EMAIL ADDRESS</label>
+                <input
+                  type="email"
+                  className="px-4 py-4  bg-rose-100  mt-1 rounded-md focus:outline-none focus:ring-2 focus:ring-rose-300 focus:border-transparent"
+                  placeholder="EMAIL"
+                  required
+                />
+              </div>
+
+              {/* Select Service */}
+              <div className="relative border-b-2 border-y-gray-300 w-full">
+                <label className="hidden">SELECT SERVICE</label>
+                <select className=" w-full required text-gray-400 px-4 py-4  bg-rose-100 mt-1 rounded-md focus:outline-none focus:ring-2 focus:ring-rose-300 focus:border-transparent">
+                  <option>SELECT SERVICE</option>
+                  <option>Service 1</option>
+                  <option>Service 2</option>
+                </select>
+              </div>
+
+              {/* Phone Number */}
+              <div className="relative border-b-2 border-y-gray-300 w-full">
+                <label className="hidden">PHONE NUMBER</label>
+                <input
+                  type="text"
+                  required
+                  className=" w-full px-4 py-4  bg-rose-100 mt-1 rounded-md focus:outline-none focus:ring-2 focus:ring-rose-300 focus:border-transparent"
+                  placeholder="PHONE NUMBER"
+                />
+              </div>
+
+              {/* Date Picker */}
+              <div className="relative border-b-2 border-y-gray-300 w-full">
+                <label className="hidden">DATE</label>
+                <input
+                  type="date"
+                  required
+                  className=" w-full px-4 py-4 text-gray-400 bg-rose-100 mt-1 rounded-md focus:outline-none focus:ring-2 focus:ring-rose-300 focus:border-transparent"
+                />
+              </div>
+
+              {/* Time Picker */}
+              <div className="relative border-b-2 border-y-gray-300 w-full">
+                <label className="hidden">TIME</label>
+                <input
+                  type="time"
+                  required
+                  className=" w-full px-4 py-4 text-gray-400 bg-rose-100 mt-1 rounded-md focus:outline-none focus:ring-2 focus:ring-rose-300 focus:border-transparent"
+                />
+              </div>
+
+              {/* Notes */}
+              <div className="col-span-2 border-b-2 border-y-gray-300 w-full">
+                <label className="hidden">YOUR NOTES</label>
+                <textarea
+                  rows="2"
+                  className=" w-full px-4 py-4  bg-rose-100 mt-1 rounded-md focus:outline-none focus:ring-2 focus:ring-rose-300 focus:border-transparent"
+                  placeholder="Add your notes here"
+                ></textarea>
+              </div>
+            </div>
+
+            {/* Button */}
+            <div className="mt-5">
+              <button
+                type="submit"
+                className="px-6 py-3 bg-rose-400 text-white rounded-full hover:bg-rose-500 transition-colors focus:outline-none focus:ring-2 focus:ring-rose-500"
+              >
+                MAKE AN APPOINTMENT
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BookAtime;
--- src/app/Team/page.jsx
+++ src/app/Team/page.jsx
@@ -1,10 +1,11 @@
 "use client";
 import React, { useEffect } from "react";
+import Image from "next/image";
 
 import useRequestData from "../../../Hooks/useRequestData";
 
 const Team = () => {
-  const { data: reviews, isLoading, error, makeRequest } = useRequestData();
+  const { data, isLoading, error, makeRequest } = useRequestData();
 
   useEffect(() => {
     makeRequest("http://localhost:5029/team");
@@ -12,31 +13,36 @@ const Team = () => {
 
   return (
     <>
-      <h1>Experienced Team</h1>
+      <h1 className="pt-16 text-center font-semibold text-3xl">
+        Experienced Team
+      </h1>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime quia,
         exercitationem cupiditate vero nulla explicabo nihil nobis culpa tempora
         iure, non accusamus quibusdam?
       </p>
 
-      <div>
+      <div className="flex flex-row justify-center items-start w-full flex-wrap">
         {data &&
-          data.map((t) => (
-            <div
-              key={t.id}
-              className="max-w-xs bg-white border border-gray-200 p-5 m-4"
-            >
-              <h3 className="text-lg font-semibold mt-4">{p.title}</h3>
-              <p
-                className="text-sm text-gray-600 mt-2 line-clamp-3 pt-4"
-                dangerouslySetInnerHTML={{ __html: p.content }}
-              ></p>
-              <button className="bg-gray-800 text-white rounded-full my-6 px-4 py-2">
-                <Link href="/Service">READ MORE</Link>
-              </button>
+          data.map((e) => (
+            <div className="max-w-sm p-5 m-4" key={e.id}>
+              <Image
+                src={"http://localhost:5029/images/team/" + e.image}
+                width={300}
+                height={150}
+                alt="photo_Team"
+              ></Image>
+              <div className="relative -mt-10 px-10 pt-5 pb-10 bg-white m-10 border border-gray-200 shadow">
+              <h3 className="font-semibold text-lg ">
+                {e.firstname} {e.lastname}
+              </h3>
+              <p>{e.role}</p>
+              </div>
             </div>
           ))}
       </div>
+
+    
     </>
   );
 };
