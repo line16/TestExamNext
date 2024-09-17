a208585f9fb7774bac17fa82524ade3f2d20beea
line16 <linesorensen53@gmail.com>
2024-09-17 14:50:41 +0200

    update

--- src/app/Recommendation/FrontpageRec/page.jsx
+++ src/app/Recommendation/FrontpageRec/page.jsx
@@ -1,57 +1,43 @@
-"use client";
+"use client"
+import React from "react";
+import Slider from "react-slick";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "./sliderStyles.css";
 
-import React, { useEffect } from "react";
-import Slider from "react-slick";
-import useRequestData from "../../../../Hooks/useRequestData";
-
-const FrontRec = () => {
-  const { data: reviews, isLoading, error, makeRequest } = useRequestData();
-
-  useEffect(() => {
-    makeRequest("http://localhost:5029/recommendation/antal/3")
-  }, [])
-  
+function SimpleSlider() {
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
+    autoPlay: true,
     slidesToShow: 1,
-    slidesToScroll: 1,
+    slidesToScroll: 1
   };
-
-
   return (
-    <div className="relative m-auto text-center text-black py-64 bg-opacity-600 bg-testimonialsBackground bg-cover">
-      {/* pink overlay */}
-      <div className="absolute top-0 left-0 h-fit w-full bg-rose-200 opacity-40 z-20"></div>
-  
-      <h1 className="text-3xl font-bold text-center mt-8 relative z-40">Kundeudtalelser</h1>
-      <hr className="w-20 border-y-2 my-4 m-auto relative z-40" />
-  
-      {isLoading ? (
-        <p className="relative z-40">Loading...</p> // Loading state
-      ) : error ? (
-        <p className="relative z-40">Error: {error.message}</p> // Error state
-      ) : reviews && reviews.length > 0 ? (
-        <Slider {...settings} className="z-50">
-          {reviews.map((review) => (
-            <div key={review._id} className="p-8">
-              <p className="text-xl">"{review.content}"</p>
-              <p className="mt-4 text-lg font-semibold">- {review.name}</p>
-              <p>{review.title}</p>
-            </div>
-          ))}
-        </Slider>
-      ) : (
-        <p className="relative z-40">No reviews found</p>
-      )}
+    <div className="slider-container">
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
     </div>
   );
-  
-};
+}
 
-export default FrontRec;
+export default SimpleSlider;
--- src/app/Team/page.jsx
+++ src/app/Team/page.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+"use client";
+import React, { useEffect } from "react";
+
+import useRequestData from "../../../Hooks/useRequestData";
 
 const Team = () => {
-  return (
-    <div>Team</div>
-  )
-}
+  const { data: reviews, isLoading, error, makeRequest } = useRequestData();
 
-export default Team
+  useEffect(() => {
+    makeRequest("http://localhost:5029/team");
+  }, []);
+
+  return (
+    <>
+      <h1>Experienced Team</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime quia,
+        exercitationem cupiditate vero nulla explicabo nihil nobis culpa tempora
+        iure, non accusamus quibusdam?
+      </p>
+
+      <div>
+        {data &&
+          data.map((t) => (
+            <div
+              key={t.id}
+              className="max-w-xs bg-white border border-gray-200 p-5 m-4"
+            >
+              <h3 className="text-lg font-semibold mt-4">{p.title}</h3>
+              <p
+                className="text-sm text-gray-600 mt-2 line-clamp-3 pt-4"
+                dangerouslySetInnerHTML={{ __html: p.content }}
+              ></p>
+              <button className="bg-gray-800 text-white rounded-full my-6 px-4 py-2">
+                <Link href="/Service">READ MORE</Link>
+              </button>
+            </div>
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default Team;
